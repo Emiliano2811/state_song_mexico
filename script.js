11a235c1f7f5fa607306a63b64f1d7f88857e0d8
Emiliano2811 <emilianotorres28@hotmail.com>
2022-09-27 18:34:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,369 @@
+/* Estoy buscando hacer una función para que con cada estado se ejecute el script tipo hover que al pasar el curso o darle
+click se reproduzca la canción. Cada estado va acompañado de Mx y las siglas del estado ejemplo MxMOR en este caso Morelos 
+y el audio es solo las siglas del estado MOR compañado de Audio MORAudio*/
+
+CDMX.addEventListener("mouseenter", e =>
+        {
+          CDMXAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        CDMX.addEventListener("mouseleave", e =>
+        {
+          CDMXAudio.pause();
+        });
+          
+          MxAGU.addEventListener("mouseenter", e =>
+        {
+          AGUAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxAGU.addEventListener("mouseleave", e =>
+        {
+          AGUAudio.pause();
+        });
+          
+          MxBCN.addEventListener("mouseenter", e =>
+        {
+          BCNAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxBCN.addEventListener("mouseleave", e =>
+        {
+          BCNAudio.pause();
+        });
+          
+          MxBCS.addEventListener("mouseenter", e =>
+        {
+          BCSAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxBCS.addEventListener("mouseleave", e =>
+        {
+          BCSAudio.pause();
+        });
+          
+           MxCAM.addEventListener("mouseenter", e =>
+        {
+          CAMAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxCAM.addEventListener("mouseleave", e =>
+        {
+          CAMAudio.pause();
+        });
+          
+          MxCHP.addEventListener("mouseenter", e =>
+        {
+          CHPAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxCHP.addEventListener("mouseleave", e =>
+        {
+          CHPAudio.pause();
+        });
+          
+          
+          MxCHH.addEventListener("mouseenter", e =>
+        {
+          CHHAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxCHH.addEventListener("mouseleave", e =>
+        {
+          CHHAudio.pause();
+        });
+        
+          
+        MxCOA.addEventListener("mouseenter", e =>
+        {
+          COAAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxCOA.addEventListener("mouseleave", e =>
+        {
+          COAAudio.pause();
+        });
+        
+          
+          MxCOL.addEventListener("mouseenter", e =>
+        {
+          COLAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxCOL.addEventListener("mouseleave", e =>
+        {
+          COLAudio.pause();
+        });
+          
+          
+          
+          MxDUR.addEventListener("mouseenter", e =>
+        {
+          DURAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxDUR.addEventListener("mouseleave", e =>
+        {
+          DURAudio.pause();
+        });
+          
+          
+          MxGUA.addEventListener("mouseenter", e =>
+        {
+          GUAAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxGUA.addEventListener("mouseleave", e =>
+        {
+          GUAAudio.pause();
+        });
+          
+          
+          MxGRO.addEventListener("mouseenter", e =>
+        {
+          GROAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxGRO.addEventListener("mouseleave", e =>
+        {
+          GROAudio.pause();
+        });
+          
+          
+          MxHID.addEventListener("mouseenter", e =>
+        {
+          HIDAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxHID.addEventListener("mouseleave", e =>
+        {
+          HIDAudio.pause();
+        });
+          
+          
+          MxJAL.addEventListener("mouseenter", e =>
+        {
+          JALAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxJAL.addEventListener("mouseleave", e =>
+        {
+          JALAudio.pause();
+        });
+          
+          
+          MxMEX.addEventListener("mouseenter", e =>
+        {
+          MEXAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxMEX.addEventListener("mouseleave", e =>
+        {
+          MEXAudio.pause();
+        });
+          
+          MxMIC.addEventListener("mouseenter", e =>
+        {
+          MICAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxMIC.addEventListener("mouseleave", e =>
+        {
+          MICAudio.pause();
+        });
+          
+          
+         MxMOR.addEventListener("mouseenter", e =>
+        {
+          MORAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxMOR.addEventListener("mouseleave", e =>
+        {
+          MORAudio.pause();
+        });
+          
+          
+          MxNAY.addEventListener("mouseenter", e =>
+        {
+          NAYAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxNAY.addEventListener("mouseleave", e =>
+        {
+          NAYAudio.pause();
+        });
+          
+          
+          
+          MxNEL.addEventListener("mouseenter", e =>
+        {
+          NELAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxNEL.addEventListener("mouseleave", e =>
+        {
+          NELAudio.pause();
+        });
+          
+          
+          
+          MxOAX.addEventListener("mouseenter", e =>
+        {
+          OAXAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxOAX.addEventListener("mouseleave", e =>
+        {
+          OAXAudio.pause();
+        });
+          
+          
+          
+          MxPUE.addEventListener("mouseenter", e =>
+        {
+          PUEAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxPUE.addEventListener("mouseleave", e =>
+        {
+          PUEAudio.pause();
+        });
+          
+          
+          
+          MxQUE.addEventListener("mouseenter", e =>
+        {
+          QUEAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxQUE.addEventListener("mouseleave", e =>
+        {
+          QUEAudio.pause();
+        });
+          
+          
+          
+          MxROO.addEventListener("mouseenter", e =>
+        {
+          ROOAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxROO.addEventListener("mouseleave", e =>
+        {
+          ROOAudio.pause();
+        });
+          
+          
+          MxSLP.addEventListener("mouseenter", e =>
+        {
+          SLPAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxSLP.addEventListener("mouseleave", e =>
+        {
+          SLPAudio.pause();
+        });
+          
+          
+          
+          MxSIN.addEventListener("mouseenter", e =>
+        {
+          SINAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxSIN.addEventListener("mouseleave", e =>
+        {
+          SINAudio.pause();
+        });
+          
+          
+          MxSON.addEventListener("mouseenter", e =>
+        {
+          SONAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxSON.addEventListener("mouseleave", e =>
+        {
+          SONAudio.pause();
+        });
+          
+          
+          
+          MxTAB.addEventListener("mouseenter", e =>
+        {
+          TABAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxTAB.addEventListener("mouseleave", e =>
+        {
+          TABAudio.pause();
+        });
+          
+          
+          
+          MxTAM.addEventListener("mouseenter", e =>
+        {
+          TAMAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxTAM.addEventListener("mouseleave", e =>
+        {
+          TAMAudio.pause();
+        });
+          
+          
+          
+          MxTLA.addEventListener("mouseenter", e =>
+        {
+          TLAAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxTLA.addEventListener("mouseleave", e =>
+        {
+          TLAAudio.pause();
+        });
+          
+          
+          MxVER.addEventListener("mouseenter", e =>
+        {
+          VERAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxVER.addEventListener("mouseleave", e =>
+        {
+          VERAudio.pause();
+        });
+          
+          
+          
+          MxYUC.addEventListener("mouseenter", e =>
+        {
+          YUCAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxYUC.addEventListener("mouseleave", e =>
+        {
+          YUCAudio.pause();
+        });
+          
+          
+          
+          
+          MxZAC.addEventListener("mouseenter", e =>
+        {
+          ZACAudio.play().catch(er => console.error(er.message, "Now, click here and try again"));
+        });
+        
+        MxZAC.addEventListener("mouseleave", e =>
+        {
+          ZACAudio.pause();
+        });
+       
+          
+          
+          
+          
+          
+          
+          
